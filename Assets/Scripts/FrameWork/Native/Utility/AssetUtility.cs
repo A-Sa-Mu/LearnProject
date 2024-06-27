7c6b309b3988223db94f258d8f46ca2d6dc5e369
zhangbiao <zhangbiao@wepie.com>
2024-06-27 17:25:42 +0800

    测试

---
+++ Assets/Scripts/FrameWork/Native/Utility/AssetUtility.cs
@@ -0,0 +1,26 @@
+﻿using GameFramework;
+using GameFramework.Resource;
+using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace FrameWork
+{
+    public static class AssetUtility
+    {
+        public static string GetDataTableAsset(string assetName,bool isBinary)
+        {
+            if (isBinary)
+            {
+                return $"Assets/AssetBundleRes/Configs/DataTables/{assetName}.bytes";
+            }
+            else
+            {
+                return $"Assets/AssetBundleRes/Configs/DataTables/{assetName}.txt";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdParty/GameFramework/TestScripts/TestDataTableComponent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+using AssetUtility = FrameWork.AssetUtility;
+using GameEntry = FrameWork.GameEntry;
+
+namespace GameFramework
+{
+    public class TestDataTableComponent : MonoBehaviour
+    {
+        private DataTableComponent DataTable;
+        private string[] DataTableNames;
+
+        private void Start()
+        {
+            DataTable = GameEntry.DataTable;
+            DataTableNames = new string[] { "Music" };
+            foreach (var tableName in DataTableNames)   // 加载表格
+            {
+                var dataTableAssetName = AssetUtility.GetDataTableAsset(tableName, false);
+                GameEntry.DataTable.LoadDataTable(tableName, dataTableAssetName, this);
+            }
+            
+            // 查询
+            var music = GameEntry.DataTable.GetDataTable<DRMusic>();
+            Debug.LogError(music.GetDataRow(1).Id);    // 第一行
+
+            var music2 = music.GetDataRow(x => x.Id == 1);  // 根据id查询
+            Debug.LogError($"{music2.AssetName}");
+        }
+    }
+}
